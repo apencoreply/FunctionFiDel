6aeeeb2ded104f67dd3268c0f2083f00accd0227
Alessandro Penco <62288664+AlessandroPenco@users.noreply.github.com>
2023-06-28 12:13:02 +0200

    Add project files.

---
+++ FiDelApp.cs
@@ -0,0 +1,76 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net.Http;
+using Microsoft.Azure.Cosmos;
+using System.ComponentModel;
+using Azure;
+using System.Collections.Generic;
+
+namespace FiDelApp
+{
+
+
+    public static class FiDelApp
+    {
+        private static readonly HttpClient httpClient = new HttpClient();
+        private static readonly string cosmosDbEndpoint = Environment.GetEnvironmentVariable("DBENDPOINT");
+        private static readonly string cosmosDbKey = Environment.GetEnvironmentVariable("DBKEY");
+        private static readonly string databaseId = Environment.GetEnvironmentVariable("DBID");
+        private static readonly string containerId = Environment.GetEnvironmentVariable("CONTAINERID");
+        private static readonly CosmosClient cosmosClient = new CosmosClient(cosmosDbEndpoint, cosmosDbKey);
+        private static readonly Microsoft.Azure.Cosmos.Container cosmosContainer = cosmosClient.GetContainer(databaseId, containerId);
+
+        [FunctionName("FiDelApp")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            try
+            {
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+                // Deserialize the JSON request body
+                var requestDocument = JsonConvert.DeserializeObject<RequestDocument>(requestBody);
+
+                // Validate that the ID is provided
+                if (string.IsNullOrEmpty(requestDocument.id))
+                {
+                    return new BadRequestObjectResult("The 'id' property is required.");
+                }
+
+                // Insert the JSON document into Cosmos DB
+                var response = await cosmosContainer.CreateItemAsync(requestDocument);
+
+                return new OkObjectResult("Data inserted successfully!");
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex, "An error occurred while inserting data into Cosmos DB.");
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+    public class RequestDocument
+    {
+        public string id { get; set; }
+        public string categoryId { get; set; }
+        public string categoryName { get; set; }
+        public string sku { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public double Price { get; set; }
+        public List<Tag> tags { get; set; }
+    }
+    public class Tag
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+}
